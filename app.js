4370804eacddca58a6c49e975861d5d4818dc011
saicharan20012007 <saicharan20012007@gmail.com>
2023-05-19 16:17:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const bodyParser = require("body-parser");
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(config);
+
+// Setup server
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({
+  origin: '*'
+}));
+// endpoint for ChatGPT
+
+app.post("/chat", async (req, res) => {
+  const { prompt } = req.body;
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    max_tokens: 512,
+    temperature: 0,
+    prompt: prompt,
+  });
+res.header('Access-Control-Allow-Origin', '*');
+  res.send(completion.data.choices[0].text);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
+
+app.get("/", (req, res) =>{
+    res.send("Test Api is working fine");
+})
